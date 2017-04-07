154f6b7cd162405f98ec1b45026aa2cc7fa714d1
Flatiron School <info@Flatirons-MacBook-Air-71.local>
2017-04-07 12:34:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort!
+    end
+  end
+end
